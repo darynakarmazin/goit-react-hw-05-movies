a039f54461ead6711b47a526143d326c11b79ec2
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-06-14 14:48:21 +0300

    Added MovieDetails page

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
 
 export function MovieDetails() {
   return (
-    <>
-      <div>MovieDetails PAGE</div>
-    </>
+    <div>
+      <h1>MovieDetails page</h1>
+      <ul>
+        <li>
+          <Link to="cast">Cast</Link>
+        </li>
+        <li>
+          <Link to="reviews">Go through the reviews</Link>
+        </li>
+      </ul>
+      <Outlet />
+    </div>
   );
 }
