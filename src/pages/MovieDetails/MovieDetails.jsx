db1c45063cf06c487d27c714dcccbaa6bdfc4754
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-06-15 01:20:40 +0300

    Updated import

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link, Outlet, useParams } from 'react-router-dom';
+import { Outlet, useParams } from 'react-router-dom';
 import {
   MovieDetailsPage,
   MovieDetailsDiv,
