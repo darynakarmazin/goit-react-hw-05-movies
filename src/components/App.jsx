dd3192397cce0e9cf8efea7b2f5bdb99c36cc073
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-06-15 17:43:59 +0300

    Updated import

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,12 @@ import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { SharedLayout } from './SharedLayout/SharedLayout';
 
-const Home = lazy(() => import('pages/Home/Home'));
-const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
-const Movies = lazy(() => import('pages/Movies/Movies'));
+const Home = lazy(() => import('../pages/Home/Home'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails/MovieDetails'));
+const Movies = lazy(() => import('../pages/Movies/Movies'));
 const Cast = lazy(() => import('./Cast/Cast'));
 const Reviews = lazy(() => import('./Reviews/Reviews'));
-const NotFound = lazy(() => import('pages/NotFound'));
+const NotFound = lazy(() => import('../pages/NotFound'));
 
 export const App = () => {
   return (
